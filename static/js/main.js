9cce1847a8b4bb0d99197f56413c113461a9636f
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2019-09-30 22:42:42 +0100

    initialized the character counting feature used on the username and password fields on the register form, also initialised the select elements used in the add_exercise form

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
     $('.collapsible').collapsible();
     $(".button-collapse").sideNav();
-});
+    $('input#username, input#password, input#instructions').characterCounter();
+    $('select').material_select();
+
+// Close displayed flash message
+    $('.close-btn').click(function() {
+        $('#flashed-message').remove();
+    });
+})
