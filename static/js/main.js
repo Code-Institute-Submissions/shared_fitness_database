b6672a25d87fde3d11762234f50ba3369c9f014c
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2019-10-01 20:39:54 +0100

    removed unneeded collapsible jquery, as no longer using collapsible

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
-    $('.collapsible').collapsible();
+$(document).ready(function() {
     $(".button-collapse").sideNav();
     $('input#username, input#password, input#instructions').characterCounter();
     $('select').material_select();
+})
 
 // Close displayed flash message
-    $('.close-btn').click(function() {
-        $('#flashed-message').remove();
-    });
-})
+$('.close-btn').click(function() {
+    $('#flashed-message').remove();
+});
+
+// Fix created by CI to sort out the issue with the select elements not working as required
+document.getElementsByClassName("matfix").addEventListener("click", function(e) {
+    e.stopPropagation();
+});
+
