df3c83933e91bb16f2cb9e3cce8a30e2e2bdff8e
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2019-10-03 15:57:06 +0100

    added the logic and functionality for the pagination element on the index/home page

--- app.py
+++ app.py
@@ -16,11 +16,18 @@ mongo = PyMongo(app)
 """ROUTES"""
 
 """ INDEX/HOME """
+# The below route will display the home page on index.html, the function below is for the pagination on the index page,
+# the function which will choose a limit for the number of exercises per page, count the total number
+# of exercises and then will then provide the logic for what exercises will be shown on each page
 @app.route('/')
 @app.route('/home', methods=['POST', 'GET'])
 def index():
-    exercises = mongo.db.exercises.find()
-    return render_template('index.html', exercises=exercises)
+    per_page = 3
+    page = int(request.args.get('page', 1))
+    total = mongo.db.exercises.count_documents({})
+    exercises = mongo.db.exercises.find().skip((page - 1) * per_page).limit(per_page)
+    pages = range(1, int(math.ceil(total / per_page)) + 1)
+    return render_template('index.html', exercises=exercises, page=page, pages=pages, total=total)
 
 
 """ SEARCH """
@@ -134,11 +141,65 @@ def insert_exercise():
 
 
 """ EXERCISE PAGE """
+# This function will render the exercise.html page which will use the exercise id to display a specific exercise
+# and its information, and allow the user that created said exercise to be able to access edit and delete functionality
 @app.route('/exercise/<exercise_id>', methods=['POST', 'GET'])
 def exercise(exercise_id):
     this_exercise = mongo.db.exercises.find_one({"_id": ObjectId(exercise_id)})
     return render_template('exercise.html',
                            exercise=this_exercise)
+
+
+""" EDIT EXERCISE PAGE """
+# This function will render the edit_exercise.html page, and will pre-fill the form fields
+# with the information previously submitted by the user, this allows them to view the exercise and its information
+# and change any fields they may need to as they see fit
+@app.route("/edit_exercise/<exercise_id>", methods=['POST', 'GET'])
+def edit_exercise(exercise_id):
+    this_exercise = mongo.db.exercises.find_one({"_id": ObjectId(exercise_id)})
+
+    muscles = mongo.db.muscles.find()
+    types_of_exercise = mongo.db.types_of_exercise.find()
+    mechanics = mongo.db.mechanics.find()
+    equipment = mongo.db.equipment.find()
+    difficulty = mongo.db.difficulty.find()
+    return render_template('edit_exercise.html',
+                           exercise=this_exercise,
+                           muscles=muscles,
+                           types_of_exercise=types_of_exercise,
+                           mechanics=mechanics,
+                           equipment=equipment,
+                           difficulty=difficulty)
+
+
+""" UPDATE EXERCISE """
+# This function allows the information in the form to be submitted to the database therefore editing the exercise
+# being accessed from exercises collection and redirecting the user back to the index page after the form has been posted
+@app.route("/update_exercise/<exercise_id>", methods=['POST'])
+def update_exercise(exercise_id):
+    updated_exercise = mongo.db.exercises.find_one({"_id": ObjectId(exercise_id)})
+    updated_fields = request.form.to_dict()
+    mongo.db.exercises.update_one(updated_exercise, {"$set": updated_fields})
+
+    return redirect(url_for('index'))
+
+
+""" DELETE EXERCISE PAGE """
+# This function will render the delete_exercise.html page
+@app.route("/delete_exercise/<exercise_id>", methods=['POST', 'GET'])
+def delete_exercise(exercise_id):
+    deleted_exercise = mongo.db.exercises.find_one({"_id": ObjectId(exercise_id)})
+
+    return render_template('delete_exercise.html', exercise=deleted_exercise)
+
+
+""" REMOVE EXERCISE """
+# This function allows the exercise to be removed from the database
+# should the user that created the exercise decide it is necessary to do so
+@app.route("/remove_exercise/<exercise_id>", methods=['POST', 'GET'])
+def remove_exercise(exercise_id):
+    mongo.db.exercises.remove({"_id": ObjectId(exercise_id)})
+    return redirect(url_for('index'))
 
 
 app.run(host=os.environ.get('IP', '127.0.0.1'),
