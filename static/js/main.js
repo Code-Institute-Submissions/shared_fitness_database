4915b0a9f38a6380353ee1ea465736517e9a3bba
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2019-10-15 12:09:41 +0100

    updated explanation for the matfix fix code put in my main.js file

--- static/js/main.js
+++ static/js/main.js
@@ -10,6 +10,8 @@ $('.close-btn').click(function() {
 });
 
 // Fix created by CI to sort out the issue with the select elements not working as required
+// When clicked on, they were originally having a pop-in/pop-out effect and had to click on the select box a couple of
+// times to get the dropdown options to stay in place, this fix solves that
 document.getElementById("matfix1").addEventListener("click", function(e) {
     e.stopPropagation();
 });
